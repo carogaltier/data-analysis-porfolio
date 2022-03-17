29d6fee49ad48461df8fb26dbd84a8beaf50e4a9
Maria Carolina Gonzalez Galtier <53412617+carogaltier@users.noreply.github.com>
2022-03-17 20:29:40 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready( function () {
+    $('#ageGroupTable19').DataTable();
+    $('#groupCausesTable19').DataTable();
+    $('#regionTable19').DataTable();
+    $('#ageGroupTable18').DataTable();
+    $('#groupCausesTable18').DataTable();
+    $('#regionTable18').DataTable();
+    $('#ageGroupTable17').DataTable();
+    $('#groupCausesTable17').DataTable();
+    $('#regionTable17').DataTable();
+} );
